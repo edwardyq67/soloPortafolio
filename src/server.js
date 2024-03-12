7cdbeff7ad5694e8d51e4d62ac22a7376eb3919e
edwardyq67 <edwardyq67@gmail.com>
2024-03-12 16:50:28 -0500

    gaads

--- src/server.js
+++ src/server.js
@@ -1,17 +1,13 @@
-const cord = require('cords');
 const app = require('./app');
 const sequelize = require('./utils/connection');
-const cors=require("cors")
 require('./models')
-const PORT = process.env.PORT || 5432;
+const PORT = process.env.PORT || 5230;
 
 const main = async () => {
     try {
-        app.use(cors())
         sequelize.sync();
         console.log("DB connected");
         app.listen(PORT);
-        
         console.log(`Server running on port ${PORT}`);
     } catch (error) {
         console.log(error)
