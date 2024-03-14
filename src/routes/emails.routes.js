0ed50672585ac3f66f7a697b192ecc6e5e8ec237
edwardyq67 <edwardyq67@gmail.com>
2024-03-14 13:13:15 -0500

    klistosa

--- src/routes/emails.routes.js
+++ src/routes/emails.routes.js
@@ -1,9 +1,10 @@
-const { sayHi } = require('../controllers/email.controllers');
 const express = require('express');
+const cors = require('cors'); // Importa el paquete cors
+const { sayHi } = require('../controllers/email.controllers');
 
 const routerEmail = express.Router();
 
-routerEmail.route('/')
-    .post(sayHi)
+// Configura CORS para la ruta 'sayHi'
+routerEmail.post('/', cors(), sayHi);
 
 module.exports = routerEmail;
