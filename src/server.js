198f79135b63d6f52be79b42d024b59ed6d30dbf
edwardyq67 <edwardyq67@gmail.com>
2024-03-19 12:24:34 -0500

    asdnu

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const sequelize = require('./utils/connection');
 require('./models')
-const PORT = process.env.PORT || 5432;
+const PORT = process.env.PORT || 5230;
 
 const main = async () => {
     try {
